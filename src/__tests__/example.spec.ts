978322e17f6f14335decc5fd417353a0d7c1f0ab
0xNeon-opensource <90662438+0xNeon-opensource@users.noreply.github.com>
2022-05-12 12:50:09 -0500

    First commit

---
+++ src/__tests__/example.spec.ts
@@ -0,0 +1,6 @@
+import { works } from '../utils/exampleUtil';
+describe('example', () => {
+  it('should work', () => {
+    expect(works).toBe(true);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
-import {
-  getCostOfARInDollars,
-  getCostToSavePathToArweaveInAR,
-  getCostToSavePathToArweaveInDollars,
-  getCostToSaveBytesToArweaveInAR,
-  getCostToSaveBytesToArweaveInDollars,
-} from './utils/costEstimator';
-import {
-  connectToLocalArweave,
-  connectToArweave,
-  mintTestWinstonsToKey,
-  generateTestKey,
-  uploadDataToArweave,
-} from './utils/arweaveUtils';
-import ArweaveNftUploader from './utils/arweaveNftUploader';
-import { WINSTONS_PER_AR } from './constants';
+import { works } from './utils/exampleUtil';
 
 export {
-  WINSTONS_PER_AR,
-  getCostOfARInDollars,
-  getCostToSavePathToArweaveInAR,
-  getCostToSavePathToArweaveInDollars,
-  getCostToSaveBytesToArweaveInAR,
-  getCostToSaveBytesToArweaveInDollars,
-  mintTestWinstonsToKey,
-  connectToLocalArweave,
-  connectToArweave,
-  generateTestKey,
-  uploadDataToArweave,
-  ArweaveNftUploader,
+  works
 };
